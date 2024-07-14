e194bd3cf3e346b3e418163802b617cccb7c2381
Iqbal Fauzan <imfauzan14@gmail.com>
2024-07-14 19:20:50 +0700

    feat: added cors

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 const chartRoute = require("./routes/chart.route");
 const sameReporterRoute = require("./routes/sameReporter.route");
 require("dotenv").config();
@@ -8,12 +9,13 @@ const superAdminRouter = require("./routes/superAdmin");
 
 const app = express();
 
+app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
-app.use('/category', categoryRouter);
-app.use("/superAdmin", superAdminRouter)
+app.use("/category", categoryRouter);
+app.use("/superAdmin", superAdminRouter);
 app.use("/api", chartRoute);
 app.use("/api", sameReporterRoute);
 
