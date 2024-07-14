179f934ac3ad9b6e13f5fa58d640f50f81f4d85a
unknown <moch.wijayarizky@gamil.com>
2024-07-14 22:08:44 +0700

    first commit

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const userRoutes = require('./routes/userRoutes'); // Adjust the path as necessary
+const userRoutes = require('./routes/userRoutes');
+const institutionRoutes = require('./routes/institutionRoutes'); // Import the institution routes
 
 const app = express();
 const port = 3000;
 
+// Middleware for parsing JSON and urlencoded form data
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -13,8 +15,12 @@ app.get('/', (req, res) => {
   res.send('Welcome to the User Profile App');
 });
 
-// Use user routes
+// Use user routes for endpoints starting with /users
 app.use('/users', userRoutes);
+
+// Use institution routes for endpoints starting with /institutions
+app.use('/institutions', institutionRoutes);
+
 
 // Start the server
 app.listen(port, () => {
